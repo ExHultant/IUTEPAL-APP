18a81728533a5361300d5d93442e6db1eee7e8e2
Ricardo Chavez <74266998+ExHultant@users.noreply.github.com>
2024-03-04 00:23:13 -0400

    Add report upload functionality

--- consultorio-medico/src/Pages/Reports/index.jsx
+++ consultorio-medico/src/Pages/Reports/index.jsx
@@ -2,19 +2,61 @@ import { Sidebar } from "../../Layout";
 import {
   AreaChart,
   BarChart,
+  Button,
   Card,
+  Dialog,
+  DialogPanel,
+  Flex,
   Grid,
   LineChart,
   Subtitle,
+  Text,
   Title,
 } from "@tremor/react";
-import { SolicitudesCita, consultorioMedicoData, pacientesEnfermedadesData } from "../../utils/data-reports/consultorio-data";
+import {
+  SolicitudesCita,
+  consultorioMedicoData,
+  pacientesEnfermedadesData,
+} from "../../utils/data-reports/consultorio-data";
+import { DocumentIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const Reports = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Sidebar pageTitle={"Reportes"}>
       <main>
+        <Dialog open={isOpen} onClose={(val) => setIsOpen(val)} static={true}>
+          <DialogPanel className="max-w-4xl">
+            <Flex alignItems="center" justifyContent="between" className="mb-4">
+              <Title className="mb-3">Registrar Reporte</Title>
+              <XMarkIcon
+                className="h-6 w-6 cursor-pointer"
+                onClick={() => setIsOpen(false)}
+              />
+            </Flex>
+            <Card className="p-10">
+              <div className="mt-10">
+                <Button
+                  variant="secondary"
+                  type="button"
+                  className={`relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-secondary focus:ring-offset-2`}
+                >
+                  <input
+                    type="file"
+                    className="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                  />
+                  <DocumentIcon className="mx-auto h-12 w-12 text-quartary" />
+                  <Text className="mt-2 block text-sm font-semibold text-quartary">
+                    Importar Excel
+                  </Text>
+                </Button>
+              </div>
+            </Card>
+          </DialogPanel>
+        </Dialog>
+        <Button onClick={() => setIsOpen(true)} className="flex justify-end mb-5">Subir Reportes</Button>
         <Card className="mb-10">
           <Title>Historial de Consultas y Exámenes (Número de casos)</Title>
           <AreaChart
@@ -46,7 +88,7 @@ const Reports = () => {
               colors={["blue"]}
               yAxisWidth={48}
             />
-          </Card>          
+          </Card>
           <Card>
             <Title>Solicitudes de Citas (2022-2023)</Title>
             <LineChart
